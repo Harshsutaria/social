d641167961f18625ae32088ede79efc6050564c1
HarshSutaria_vsspl <harshrajesh.sutaria@vasudhaika.net>
2022-11-11 10:32:55 +0530

    basic update in dynamo library

--- src/api/profile/profile-dao.js
+++ src/api/profile/profile-dao.js
@@ -1,20 +1,56 @@
 const postgres = require("../../../utils/postgresDB/postgres-singledb");
 const constants = require("../../constants/service-constants");
+const dynamo = require("../../../utils/Dynamo");
 
 const dao = {};
 
-dao.getUserData = async function (id) {
-  console.log("INSIDE getUserData WITH", id);
-  let result;
-  try {
-    result = await dynamo.GetDataJson(id);
-  } catch (err) {
-    console.log("GETTING ERROR WHILE FETCHING DATA FROM DYNAMO", err);
-    throw new Error(`RECORD NOT FOUND ${err}`);
-  }
+dao.createUser = async (UserData) => {
+  console.log("INSIDE CREATE USER DAO LAYER WITH", JSON.stringify(UserData));
+  //insert in postgres
+  await dao.createUserInPostgres(UserData);
+  //insert in dynamo
+  await dao.insertUpdateInDynamo(UserData.id, UserData);
+  console.log("USER CREATION SUCCESSFULLY COMPLETED");
+  return UserData;
 };
 
-dao.createUser = async function (UserData) {
+dao.updateUser = async (UserData) => {
+  console.log("INSIDE update USER DAO LAYER WITH", JSON.stringify(UserData));
+  //insert in postgres
+  await dao.updateUserInPostgres(UserData);
+  //insert in dynamo
+  await dao.insertUpdateInDynamo(UserData.id, UserData);
+  console.log("USER CREATION SUCCESSFULLY COMPLETED");
+  return UserData;
+};
+
+dao.getUser = async (id) => {
+  console.log("INSIDE GET USER DAO LAYER WITH", JSON.stringify(id));
+  let result;
+  try {
+    result = await dao.getUserFromDynamo(id);
+  } catch (error) {
+    console.log("GETTING ERROR WHILE FETCHING DATA FROM DYNAMO");
+  }
+  console.log("Fetched user successfully", JSON.stringify(result));
+  return result;
+};
+
+dao.insertUpdateInDynamo = async function (id, data) {
+  console.log("INSIDE insertUpdateInDynamo WITH", id);
+  let result;
+
+  try {
+    result = await dynamo.putItem(id, data, constants.DYNAMO_PROFILE_TABLE);
+  } catch (err) {
+    console.log("GETTING ERROR WHILE UPDATING DATA FROM DYNAMO", err);
+    throw new Error(`RECORD NOT FOUND ${err}`);
+  }
+  console.log("USER PROFILE UPDATED IN DYNAMO");
+  return result;
+};
+
+dao.createUserInPostgres = async function (UserData) {
   console.log("INSIDE CREATE USER DAO LAYER WITH", JSON.stringify(UserData));
 
   let query = `Insert into ${constants.PG_PROFILE_TABLE} (id,name,emailId,gender,state,ct,image,description,lut) values($1,$2,$3,$4,$5,$6,$7,$8,$9)`;
@@ -40,7 +76,7 @@ dao.createUser = async function (UserData) {
   return result;
 };
 
-dao.updateUser = async function (UserData) {
+dao.updateUserInPostgres = async function (UserData) {
   console.log("INSIDE UPDATE USER DAO LAYER WITH", JSON.stringify(UserData));
 
   let query = `update ${constants.PG_PROFILE_TABLE} set , name = $2  , description = $3 , state = $4 , image=$5 ,lut = $6  where id:$1`;
@@ -63,4 +99,26 @@ dao.updateUser = async function (UserData) {
   return result;
 };
 
+dao.getUserFromDynamo = async function (id) {
+  console.log("INSIDE getUserFromDynamo WITH", id);
+  let result;
+  try {
+    result = await dynamo.getItem(id, constants.DYNAMO_PROFILE_TABLE);
+  } catch (err) {
+    console.log("GETTING ERROR WHILE FETCHING DATA FROM DYNAMO", err);
+    throw new Error(`RECORD NOT FOUND ${err}`);
+  }
+  console.log("USER PROFILE UPDATED IN DYNAMO");
+  return result;
+};
+
+// dynamo.putItem(
+//   "1",
+//   { name: "share a thought ", type: "post" },
+//   constants.DYNAMO_PROFILE_TABLE
+// );
+
+// dynamo.getItem("1", constants.DYNAMO_PROFILE_TABLE).then((data) => {
+//   console.log("data is ", JSON.stringify(data.name));
+// });
 module.exports = dao;
--- src/constants/service-constants.js
+++ src/constants/service-constants.js
@@ -1,5 +1,6 @@
 let serviceConstants = {
   PG_PROFILE_TABLE: "profile",
+  DYNAMO_PROFILE_TABLE: "profile",
 };
 
 module.exports = serviceConstants;
